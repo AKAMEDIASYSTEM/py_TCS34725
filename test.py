35d8f9894c817526301f55a6dadc6e687fb7fdca
Noah Feehan <nfeehan@gmail.com>
2013-08-27 10:24:09 -0400

    Cleaning up typos and other dumb stuff

--- test.py
+++ test.py
@@ -5,6 +5,29 @@ import time
 
 tcs = tcs.TCS34725()
 tcs.begin()
+print 'gain 1'
+tcs.setGain(1)
+print tcs.getRawData()
+
+print 'gain 2'
+tcs.setGain(2)
+print tcs.getRawData()
+
+print 'gain 3'
+tcs.setGain(3)
+print tcs.getRawData()
+
+print 'gain 0'
+tcs.setGain(0)
+print tcs.getRawData()
+
+print 'gain 4, this should not work'
+tcs.setGain(4)
+print tcs.getRawData()
+
+print 'set integration time 0xFF'
+tcs.setIntegrationTime(0xFF)
+
 while True:
 	print tcs.getRawData()
 	time.sleep(1)
